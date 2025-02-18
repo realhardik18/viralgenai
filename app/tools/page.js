9f3bf796df912f359b87eb8f6c139d574d873098
hardik singh <realhardik18@gmail.com>
2025-02-18 23:25:11 +0530

    project setup

---
+++ app/tools/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Tools() {
+  return (    
+    <>
+    <p className='flex items-center justify-center text-4xl'>Welcome to Tools</p>
+    </>
+  )
+}
+
+export default Tools
